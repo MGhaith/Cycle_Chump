666e2e1288de1a66001735c5115e663ee4e9d7e0
Ghaith Magherbi <retr0creativegm@gmail.com>
2024-06-14 12:30:04 +0200

    made the bike a rigidbody

--- PlayerBike.gd
+++ PlayerBike.gd
@@ -1,16 +1,13 @@
-extends VehicleBody3D
+extends RigidBody3D
 
 @onready var body_mesh = $Bike
-@onready var front_wheel = $front/FrontWheel
+@onready var front_wheel = $FrontWheel
 
-@export var bike_speed = 5.0
-@export var bike_max_speed = 8.0
+@export var bike_speed = 20.0
+@export var bike_max_speed = 25.0
 
-@export var steer_angle = 20.0
-var steer_speed = 3.0
-
-func  _ready():
-	constant_force.z = bike_speed
+@export var steering = 18.0
+var steer_speed = 0.1
 
 func _unhandled_input(event):
 	if event.is_action_pressed("sprint"):
@@ -18,9 +15,7 @@ func _unhandled_input(event):
 
 func _physics_process(_delta):
 	
-	var input_dir = Input.get_axis("right", "left") * deg_to_rad(steer_angle)
-	steering = lerp_angle(steering, input_dir, steer_speed)
-	#front_wheel.rotation.y = input_dir
-	#body_mesh.rotate(lerp(body_mesh.rotation_degrees, front_wheel.rotation_degrees, delta))
+	var input_dir = Input.get_axis("right", "left") * deg_to_rad(steering)
+	front_wheel.rotation.y = lerp(front_wheel.rotation.y, input_dir, .1)
 	
-	#move_and_slide()
+	apply_force(front_wheel.basis.z * bike_speed)
