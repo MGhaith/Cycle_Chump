885bb0adab4b104ded70e68b9843b365012d9fea
BQKdev <bqkdev.official@gmail.com>
2024-06-14 18:57:12 -0500

    Bike Physics Finished
    A wheel rotation, and more..

--- scripts/player_bike.gd
+++ scripts/player_bike.gd
@@ -1,39 +1,75 @@
-extends CharacterBody3D
+extends VehicleBody3D
 
 @export_category("Bike Mesh")
-@export var front_wheel : Node
+@export var front_wheel_mesh : MeshInstance3D
+@export var front_wheel : VehicleWheel3D
+@export var rear_wheel : VehicleWheel3D
 
 @export_category("Bike Values")
+@export var steer_angle = 20.0
+@export var steer_speed = 2.0
+
+@export_category("Bike Speeds Mesure In Kilometres")
 @export var bike_normal_speed = 5.0
-@export var bike_fast_speed = 10.0
+@export var bike_slow_speed = 10.0
 
 var bike_current_speed = 5.0
 
-var steering = 0
+var is_on_ground = false
+var is_steering_lean = false
 
 func _physics_process(delta):
 	 # sets the steering amount base on the speed of the bike
-	steering = (bike_current_speed - 2)
 	
 	# gets the input axis (positive, negative)
-	var input_dir = Input.get_axis("right", "left") * deg_to_rad(steering)
+	var input_dir = Input.get_axis("right", "left")
 	
 	# a function that helps change the speed
 	if Input.is_action_pressed("shift"):
-		bike_current_speed = bike_fast_speed
+		bike_current_speed = bike_slow_speed
 	else:
 		bike_current_speed = bike_normal_speed
 	
 	
-	rotate_y(input_dir / bike_current_speed)
+	engine_force = lerp(engine_force, bike_current_speed, delta )
+	print(deg_to_rad(steer_angle))
 	
-	var direction = Vector3.ZERO
+	steering = lerp_angle(steering, input_dir * deg_to_rad(steer_angle), steer_speed * delta)
 	
-	direction = lerp(direction, (transform.basis * Vector3(input_dir, 0, 1)).normalized(), delta * bike_current_speed)
+	var target_rotation = input_dir * steer_angle
+	front_wheel_mesh.rotation_degrees.y = lerp(front_wheel_mesh.rotation_degrees.y, target_rotation, steer_speed * delta)
+
+func _integrate_forces(state):
 	
-	velocity = velocity.normalized()
-	velocity.x = direction.x * bike_current_speed 
-	velocity.z = direction.z * bike_current_speed
+	if front_wheel.is_in_contact() or rear_wheel.is_in_contact():
+		is_on_ground = true
+	else:
+		is_on_ground = false
 	
-	front_wheel.rotation.y = input_dir
-	move_and_slide()
+	var current_velocity = state.transform.basis.inverse() * linear_velocity
+	var negative_vel = current_velocity.z
+	
+	
+	if negative_vel >= 15:
+		is_steering_lean = true
+	elif negative_vel < 13:
+		is_steering_lean = false
+	
+	print(is_steering_lean, "negative_vel ", negative_vel, angular_velocity)
+	
+	var input_dir = Input.get_axis("right", "left")
+	
+	if is_on_ground:
+		if is_steering_lean:
+			angular_velocity = lerp(angular_velocity, -state.transform.basis.z * input_dir, 0.1)
+			steering = lerp(steering, rotation.z / 2, 0.1)
+			print("a")
+		elif abs(rotation_degrees.z) >= 1:
+			angular_velocity = lerp(angular_velocity, -state.transform.basis.z * sign(rotation_degrees.z), 0.1)
+			print("b")
+		else:
+			angular_velocity.x = 0
+			angular_velocity.z = 0
+			print("c")
+	
+	
