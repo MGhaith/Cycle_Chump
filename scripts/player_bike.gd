cbe7422efaff00416f1187475e15c8a48d80e8bb
Ghaith Magherbi <retr0creativegm@gmail.com>
2024-06-15 11:18:49 +0200

    Revert "Merge branch 'main' into car_ai"
    This reverts commit 9df4f572d05eb058334645ba333347bede2072e3, reversing
    changes made to 23ce53f4c963ca32392ac5c3db16ebcbab0f0f70.

--- scripts/player_bike.gd
+++ scripts/player_bike.gd
@@ -1,54 +1,35 @@
 extends VehicleBody3D
 
-@onready var AnimationController = $BicycleMesh/AnimationPlayer
-@onready var stamina_timer = $StaminaTimer
-@onready var visible_on_screen_enabler_3d = $VisibleOnScreenEnabler3D
-
-@export_category("Bike Properties")
-@export var camara_world : Camera3D
-
 @export_category("Bike Mesh")
 @export var front_wheel_mesh : MeshInstance3D
 @export var front_wheel : VehicleWheel3D
 @export var rear_wheel : VehicleWheel3D
 
-#_________
-
-@export_category("Bike Steer Values")
-@export var steer_angle = 45.0
-@export var steer_speed = 15.0
-
-#_________
-
-@export_category("Bike Speeds Mesure In Kilometres")
-@export var bike_normal_speed = 15.0
-@export var bike_max_speed = 25.0
-
-#_________
+@export_category("Bike Values")
+@export var steer_angle = 28.0
+@export var steer_speed = 5.0
 
 @export_category("Stamina System")
 @export var current_stamina = 100.0
-@export var stamina_gain_per_banana = 10
-
-## This max stamina remover helps when the player is using the bike full speed
 @export var max_stamina_remover = 5.0
+## This max stamina remover helps when the player is using the bike full speed
 
-## This min stamina remover helps when the player is using the bike at normal speed
 @export var min_stamina_remover = 2.0
+## This min stamina remover helps when the player is using the bike at normal speed
 
-var bike_current_speed = bike_normal_speed
+
+
+@export_category("Bike Speeds Mesure In Kilometres")
+@export var bike_normal_speed = 20.0
+@export var bike_max_speed = 30.0
+
+@onready var AnimationController = $BicycleMesh/AnimationPlayer
+@onready var stamina_timer = $StaminaTimer
+
+var bike_current_speed = 0.0
 
 var is_on_ground = false
 var is_steering_lean = false
-
-var is_bike_flipping = false
-
-func _ready():
-	if not camara_world:
-		printerr("Player needs a camara to work, assign the camara on player bike properties")
-	else:
-		visible_on_screen_enabler_3d.screen_exited.connect(Callable(camara_world, "_on_player_screen_exited"))
-		visible_on_screen_enabler_3d.screen_entered.connect(Callable(camara_world, "_on_player_screen_entered"))
 
 func _stamina_timer():
 	if bike_current_speed > bike_normal_speed:
@@ -106,37 +87,7 @@ func _integrate_forces(state):
 		else:
 			angular_velocity.x = 0
 			angular_velocity.z = 0
-
-func _banana_area_entered(area_rid, area, area_shape_index, local_shape_index):
-	if area.is_in_group("banana"):
-		print("Gain stamina")
-		var banana_children = area.get_children()
-
-		# Play the animations "picked" from the banana before queue_freeing
-		for child in banana_children:
-			if child is AnimationPlayer:
-				var banana_animations : AnimationPlayer = child
-				banana_animations.play("picked")
-
-		var preview_stamina = current_stamina + stamina_gain_per_banana
 	
-		if preview_stamina > 100:
-			current_stamina = 100
-		else:
-			current_stamina + stamina_gain_per_banana
-
-		await get_tree().create_timer(1).timeout
-		area.queue_free()
+	
 
 
-func _input(event):
-	if event.is_action_pressed("space") and !is_bike_flipping:
-		is_bike_flipping = true
-		var tween = get_tree().create_tween()
-		engine_force = 0
-		tween.tween_property(self, "position", Vector3(position.x, position.y + 0.5, position.z), 0.5)
-		tween.tween_property(self, "rotation_degrees:y", rotation_degrees.y + 180, 0.5)
-		engine_force = 10
-		tween.play()
-		await get_tree().create_timer(2).timeout
-		is_bike_flipping = false
