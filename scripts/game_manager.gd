f2dfa8f682d25754ef29fda487aa1f4c1b01a8ba
Ghaith Magherbi <retr0creativegm@gmail.com>
2024-06-15 13:11:19 +0200

    added simple ui for score and some buildings

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,13 +2,20 @@ extends Node
 class_name GameManager
 
 @export var points_node: Node
+@export var score_value_ui: Label
+@export var points_left_ui: Label
 
 var player_score: int
+var points_left: int = 0
 
 func _ready():
+	points_left = len(points_node.get_children()) 
+	
 	if points_node != null:
 		for point in points_node.get_children():
 			point.game_manager = self
+	
+	update_hud()
 	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	print_debug("Game Manager Ready")
@@ -21,4 +28,11 @@ func _process(_delta):
 
 func change_score(value : int):
 	player_score += value
-	print(str(player_score))
+	points_left -= 1
+	update_hud()
+
+func update_hud():
+	
+	score_value_ui.text = str(player_score)
+	points_left_ui.text = str(points_left)
+	
