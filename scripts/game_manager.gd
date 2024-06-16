92d0e94d071d1c6ed6e50459da297b04d11ea1cc
Ghaith Magherbi <retr0creativegm@gmail.com>
2024-06-16 07:33:30 +0200

    new changes to game manager and game scene

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,21 +3,9 @@ class_name GameManager
 
 signal level_done
 
-@export var points_node: Node
-@export var score_value_ui: Label
-@export var highest_score_ui: Label
-@export var points_left_ui: Label
-
-var player_score: int = 0
 var player_highest_score: int = 0
-var points_left: int = 0
 
 func _ready():
-	#points_left = len(points_node.get_children()) 
-	#
-	#if points_node != null:
-		#for point in points_node.get_children():
-			#point.game_manager = self
 	
 	#update_hud()
 	
@@ -28,27 +16,6 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
-
-
-func change_score(value : int):
-	player_score += value
-	points_left -= 1
-	
-	if player_score > player_highest_score:
-		player_highest_score = player_score
-	
-	# The level is cleared
-	if points_left == 0:
-		emit_signal("level_done")
-	
-	update_hud()
-
-func update_hud():
-	
-	score_value_ui.text = str(player_score)
-	points_left_ui.text = str(points_left)
-	highest_score_ui.text = str(player_highest_score)
-	
 
 
 func _on_game_won():
--- scripts/points_script.gd
+++ scripts/points_script.gd
@@ -1,24 +1,19 @@
 extends Area3D
 
 @export var pickup_player: AudioStreamPlayer
+@export var level_root: Node3D
 
-var game_manager: GameManager
 var point_value: int = 10
 
 func _physics_process(_delta):
-	if game_manager == null:
+	if level_root == null:
 		return
 	
 	if has_overlapping_bodies():
 		var overlaping_bodies = get_overlapping_bodies()
 		for body in overlaping_bodies:
 			if body.is_in_group("player"):
-				game_manager.change_score(point_value)
+				level_root.change_score(point_value)
 				pickup_player.play()
 				self.queue_free()
-	
 
-
-
-#func _on_pick_player_finished():
-	#()
